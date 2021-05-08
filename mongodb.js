ece8095f1368cdafb9900708dfa1f1bdcb21550b
Orkun Seyman <orkunseyman@gmail.com>
2021-05-08 23:04:57 +0300

    connect with mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,20 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient
+const {MongoClient,ObjectID} = require('mongodb')
 
-const connectionURL = 'mongodb://127.0.0.1.27017'
-const databaseName = 'tash-manager'
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL,{useNewUrlParser:true},(err,db)=>{
+const id = new ObjectID()
+console.log(id.getTimestamp());
+MongoClient.connect(connectionURL, { useNewUrlParser: true,useUnifiedTopology: true}, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+
+     const db = client.db(databaseName)
     
+    db.collection('tasks').find({completed:true}).toArray((error,number)=>{
+        console.log(number);
+    })
+
+
 })
